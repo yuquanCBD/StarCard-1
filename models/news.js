5194fa825d3dc2155840c14b86c06a63277200a8
hzwanggaige <hzwanggaige@corp.netease.com>
2015-09-04 20:41:27 +0800

    update all

--- models/news.js
+++ models/news.js
@@ -5,13 +5,13 @@ function News(news){
 News.add = function(obj, callback){
 	var title = obj.title;
 	var source = obj.source;
-	var create_time = obj.create_time;
+	//var create_time = obj.create_time;
 	var content = obj.content;
 	mysql.getConnection(function(err, conn){
 		if(err)
 			return callback(err);
-		var sql = 'INSERT INTO news (title, source, create_time, content) VALUES(?, ?, ?, ?)';
-		conn.query(sql, [title, source, create_time, content], function(err, results){
+		var sql = 'INSERT INTO news (title, source, content) VALUES(?, ?, ?)';
+		conn.query(sql, [title, source, content], function(err, results){
 			if(err)
 				return callback(err);
 			conn.release();
@@ -20,17 +20,35 @@ News.add = function(obj, callback){
 
 	});
 }
-News.update = function(obj, callback){
-	var news_id = obj.news_id;
+News.addMessage = function(obj, callback){
 	var title = obj.title;
-	var source = obj.source;
-	var create_time = obj.create_time;
+	//var source = obj.source;
+	//var create_time = obj.create_time;
 	var content = obj.content;
 	mysql.getConnection(function(err, conn){
 		if(err)
 			return callback(err);
-		var sql = 'update news set title= ? , source = ? , create_time= ? , content= ? where news_id= ?';
-		conn.query(sql, [title, source, create_time, content, news_id], function(err, results){
+		var sql = 'INSERT INTO manager_message (title, content) VALUES(?, ?)';
+		conn.query(sql, [title, content], function(err, results){
+			if(err)
+				return callback(err);
+			conn.release();
+			return callback(err,results);
+        });
+
+	});
+};
+News.update = function(obj, callback){
+	var news_id = obj.news_id;
+	var title = obj.title;
+	var source = obj.source;
+	//var create_time = obj.create_time;
+	var content = obj.content;
+	mysql.getConnection(function(err, conn){
+		if(err)
+			return callback(err);
+		var sql = 'update news set title= ? , source = ? , content= ? where news_id= ?';
+		conn.query(sql, [title, source, content, news_id], function(err, results){
 			if(err)
 				return callback(err);
 			conn.release();
@@ -43,7 +61,21 @@ News.getlist = function(callback){
 	mysql.getConnection(function(err, conn){
 		if(err)
 			return callback(err);
-		var sql = 'select * from news';
+		var sql = 'select * from news order by create_time DESC';
+		conn.query(sql, function(err, results){
+			if(err)
+				return callback(err);
+			conn.release();
+			return callback(err,results);
+        });
+
+	});
+};
+News.getlistMessage = function(callback){
+	mysql.getConnection(function(err, conn){
+		if(err)
+			return callback(err);
+		var sql = 'select * from manager_message order by create_time DESC';
 		conn.query(sql, function(err, results){
 			if(err)
 				return callback(err);
@@ -59,6 +91,20 @@ News.delete = function(news_id, callback){
 		if(err)
 			return callback(err);
 		var sql = 'delete from news where news_id="'+news_id+'"';
+		conn.query(sql, function(err, results){
+			if(err)
+				return callback(err);
+			conn.release();
+			return callback(err,results);
+        });
+
+	});
+}
+News.deleteMessage = function(message_id, callback){
+	mysql.getConnection(function(err, conn){
+		if(err)
+			return callback(err);
+		var sql = 'delete from manager_message where message_id="'+message_id+'"';
 		conn.query(sql, function(err, results){
 			if(err)
 				return callback(err);
